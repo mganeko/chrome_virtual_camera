ba0dc90e3b47a1c300ec248c1cb04e780371323e
masashi.g <masashi.g@gmail.com>
2020-05-24 17:26:07 +0900

    remove not used code

--- loader.js
+++ loader.js
@@ -1,32 +1,18 @@
 async function load() {
-  _loaderlog('loader load()');
-  _loaderlog('GUM2:', navigator.mediaDevices.getUserMedia);
   const res = await fetch(chrome.runtime.getURL('cs.js'), { method: 'GET' })
-  _loaderlog('loader load() after fetch');
   const js = await res.text()
-  _loaderlog('loader load() after res.text()');
   const script = document.createElement('script')
   script.textContent = js
-  _loaderlog('loader load() after createElement(script');
   document.body.insertBefore(script, document.body.firstChild)
-  _loaderlog('loader load() END');
 
   // --- tfjs ---
   const res_tf = await fetch('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2', { method: 'GET' })
-  _loaderlog('loader load() after fetch tf.js');
   const js_tf = await res_tf.text();
-  _loaderlog('loader load() after res_tf.text()');
   const script_tf = document.createElement('script');
-  //script_tf.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2';
   script_tf.textContent = js_tf;
 
   // --- bodypix ---
   const script_bp = document.createElement('script');
-  // const res_bp = await fetch('https://cdn.jsdelivr.net/npm/@tensorflow-models/body-pix@2.0', { method: 'GET' })
-  // _loaderlog('loader load() after fetch bodypix.js');
-  // const js_bp = await res_bp.text();
-  // _loaderlog('loader load() after js_bp.text()');
-  // script_bp.textContent = js_bp;
   script_bp.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/body-pix@2.0';
 
   document.body.insertBefore(script_bp, document.body.firstChild);
@@ -51,14 +37,5 @@ window.addEventListener('load', async (evt) => {
 }, true) // use capture
 
 _loaderlog('loader.js');
-
-if (!navigator.mediaDevices._originalGetUserMedia) {
-  _loaderlog('loader replacing');
-  navigator.mediaDevices._originalGetUserMedia = navigator.mediaDevices.getUserMedia;
-  _loaderlog('GUM0:', navigator.mediaDevices.getUserMedia);
-  navigator.mediaDevices.getUserMedia = null; //function () { _loaderlog('NONE GUM') };
-  // ここで入れ替えても、もとのindex.htmlでは置き換わっていない
-  _loaderlog('GUM1:', navigator.mediaDevices.getUserMedia);
-}
 
 
